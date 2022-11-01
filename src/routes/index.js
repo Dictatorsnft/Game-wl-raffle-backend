8eab70042737cca4390b70357322308b76c684b2
superstar323 <carlosarthur0323@gmail.com>
2022-11-01 23:36:54 +0800

    first

---
+++ src/routes/index.js
@@ -0,0 +1,42 @@
+import routerx from "express-promise-router";
+import {
+  create,
+  get,
+  buy,
+  getWinner,
+  checkNFT,
+  createUser,
+  updateUserWallet,
+  newWL,
+  imageMulti,
+  Uploadfiles,
+  saveHash,
+  getHistory
+} from "../controllers/raffleController";
+
+const express = require("express");
+const multer = require("multer");
+const FileUploader = require("../upload.js");
+const path = require("path");
+// trending
+const fileconfig = require("../dir");
+const Uploader = new FileUploader(path.join(fileconfig.BASEURL));
+
+const router = routerx();
+router.post("/create-user", createUser);
+router.post("/check-wallet", updateUserWallet);
+router.post("/create", create);
+router.post(
+  "/newWL",
+  multer({ storage: Uploader.storage, fileFilter: Uploader.filter }).any(),
+  imageMulti,
+  newWL
+);
+router.get("/get", get);
+router.get("/getHistory", getHistory);
+router.post("/get_winner", getWinner);
+router.post("/buy", buy);
+router.post("/check-id", checkNFT);
+router.post("/saveHash", saveHash);
+
+export default router;
